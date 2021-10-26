2f561e3de986b00afa84b4496c6d6f8287f466a5
Fernando Zavatto <zavatto.fer@gmail.com>
2021-10-26 01:38:37 -0300

    added pm2 cmds to package.json, reworked indexWrapper & added ecosystem + nginx.conf

--- 37-clase30/src/indexWraper.js
+++ 37-clase30/src/indexWraper.js
@@ -1,16 +1,15 @@
 import {args_parse} from "./config/args_parser.js";
 import {exec} from "child_process";
-import app from "./ej29.js";
-import os from "os";
+import app from "./ej30.js";
 
-const cmdFork = `pm2 start ./src/indexWraper.js --name="serverFork" --watch -f`;
-const cmdCluster = `pm2 start ./src/indexWraper.js --name="serverCluster" --watch -i 0 -f`;
+const cmdFork = `pm2 start ./src/ej30.js --name=serverFork --watch -f -- 8080`;
+const cmdCluster = `pm2 start ./src/ej30.js --name=serverCluster --watch -i 0 -f -- 8081`;
 
 const args = args_parse();
 console.log(args)
 
-if (args.mode === "cluster"){
-    exec(cmdCluster, (err, stdout, stderr) => {
+const execWrap = (arg) => {
+    exec(arg, (err, stdout, stderr) => {
         if (err || stderr) {
             if (err) console.log(`ERROR err: ${err.message}`);
             if (stderr) console.log(`ERROR: ${stderr}`);
@@ -19,19 +18,7 @@ if (args.mode === "cluster"){
         console.log(stdout);
         console.log(process.pid);
         app;
-    })
-}else{
-    [...Array(os.cpus().length).keys()] // sin esto crea 1 solo proceso fork :think:
-        .map((i) => {
-            exec(cmdFork, (err, stdout, stderr) => {
-                if (err || stderr) {
-                    if (err) console.log(`ERROR err: ${err.message}`);
-                    if (stderr) console.log(`ERROR: ${stderr}`);
-                    return;
-                }
-                console.log(stdout);
-                console.log(process.pid);
-                app;
-            });
-        });
+    });
 }
+
+(() => {args.mode === "cluster" ? execWrap(cmdCluster) : execWrap(cmdFork)})();
